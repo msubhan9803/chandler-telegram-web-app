01536fbf17a9293e71d06a09471283c1cc4f3e79
M.SUBHAN <m.subhan@MSUBHANs-MacBook-Pro.local>
2023-04-13 16:40:04 +0500

    debug stringify added

--- src/pages/accept-trade.tsx
+++ src/pages/accept-trade.tsx
@@ -1,26 +1,42 @@
 import type { NextPage } from "next";
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from "react";
 import { ProgressBar } from "react-bootstrap";
 import BuyAllHeaderDiv from "@/components/buy-all-header-div";
 import ReceiveAddressForm from "@/components/receive-address-form";
 import Button from "@/components/button";
 
 const CreateTrade: NextPage = () => {
+  const [state, setState] = useState({
+    userId: '',
+    name: '',
+    username: '',
+    languageCode: '',
+    queryId: '',
+  });
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
 
-    const userId = urlParams.get("user_id");
-    const name = urlParams.get("name");
-    const username = urlParams.get("username");
-    const languageCode = urlParams.get("language_code");
-    const queryId = urlParams.get("query_id");
+    const userId = urlParams.get("user_id") as string;
+    const name = urlParams.get("name") as string;
+    const username = urlParams.get("username") as string;
+    const languageCode = urlParams.get("language_code") as string;
+    const queryId = urlParams.get("query_id") as string;
 
     console.log(userId, name, username, languageCode, queryId);
+    setState({
+      userId,
+      name,
+      username,
+      languageCode,
+      queryId,
+    });
   }, []);
 
   return (
     <div className="relative w-full overflow-hidden flex flex-col items-center justify-center">
       <div className="rounded-xl w-full bg-dark-gray shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] box-border overflow-y-auto flex flex-col p-[18px] items-center justify-start gap-[9px] border-[2.5px] border-solid border-zinc-700">
+        {JSON.stringify(state)}
+
         <div className="flex w-full md:w-3/4 lg:w-1/2 flex-col p-[9px] items-center justify-start gap-[18px]">
           <BuyAllHeaderDiv />
           <div className="flex flex-col items-start justify-start gap-[18px] w-full">
