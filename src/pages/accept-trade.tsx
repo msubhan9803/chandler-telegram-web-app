bf6ab42de43d652083adb459ccc6421032501652
M.SUBHAN <m.subhan@MSUBHANs-MacBook-Pro.local>
2023-04-13 17:56:17 +0500

    telegram debug

--- src/pages/accept-trade.tsx
+++ src/pages/accept-trade.tsx
@@ -5,6 +5,12 @@ import BuyAllHeaderDiv from "@/components/buy-all-header-div";
 import ReceiveAddressForm from "@/components/receive-address-form";
 import Button from "@/components/button";
 
+declare global {
+  interface Window { Telegram: any; }
+}
+
+window.Telegram = window.Telegram || {};
+
 const CreateTrade: NextPage = () => {
   const [state, setState] = useState({
     userId: '',
@@ -38,6 +44,7 @@ const CreateTrade: NextPage = () => {
     <div className="relative w-full overflow-hidden flex flex-col items-center justify-center">
       <div className="rounded-xl w-full bg-dark-gray shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] box-border overflow-y-auto flex flex-col p-[18px] items-center justify-start gap-[9px] border-[2.5px] border-solid border-zinc-700">
         {JSON.stringify(state)}
+        {JSON.stringify(window?.Telegram?.WebApp)}
 
         <div className="flex w-full md:w-3/4 lg:w-1/2 flex-col p-[9px] items-center justify-start gap-[18px]">
           <BuyAllHeaderDiv />
