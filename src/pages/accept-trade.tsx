b84b3aa575f982019d8fe8625554db063b0d951f
M.SUBHAN <m.subhan@MSUBHANs-MacBook-Pro.local>
2023-04-13 14:56:52 +0500

    url params

--- src/pages/accept-trade.tsx
+++ src/pages/accept-trade.tsx
@@ -1,22 +1,31 @@
 import type { NextPage } from "next";
+import React, { useEffect } from 'react';
 import { ProgressBar } from "react-bootstrap";
 import BuyAllHeaderDiv from "@/components/buy-all-header-div";
 import ReceiveAddressForm from "@/components/receive-address-form";
 import Button from "@/components/button";
 
 const CreateTrade: NextPage = () => {
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const userId = urlParams.get("user_id");
+    const name = urlParams.get("name");
+    const username = urlParams.get("username");
+    const languageCode = urlParams.get("language_code");
+    const queryId = urlParams.get("query_id");
+
+    console.log(userId, name, username, languageCode, queryId);
+  }, []);
+
   return (
     <div className="relative w-full overflow-hidden flex flex-col items-center justify-center">
       <div className="rounded-xl w-full bg-dark-gray shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] box-border overflow-y-auto flex flex-col p-[18px] items-center justify-start gap-[9px] border-[2.5px] border-solid border-zinc-700">
         <div className="flex w-full md:w-3/4 lg:w-1/2 flex-col p-[9px] items-center justify-start gap-[18px]">
           <BuyAllHeaderDiv />
           <div className="flex flex-col items-start justify-start gap-[18px] w-full">
-            <ReceiveAddressForm
-              cryptoCurrencyText="BTC"
-            />
-            <ReceiveAddressForm
-              cryptoCurrencyText="ETH"
-            />
+            <ReceiveAddressForm cryptoCurrencyText="BTC" />
+            <ReceiveAddressForm cryptoCurrencyText="ETH" />
           </div>
           <ProgressBar striped={true} />
         </div>
