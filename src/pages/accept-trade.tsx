633d4541321f63391de7e8c7c58b5ad314420e62
msubhan9803 <mohammadsubhan9803@gmail.com>
2023-04-13 19:11:10 +0500

    all

--- src/pages/accept-trade.tsx
+++ src/pages/accept-trade.tsx
@@ -20,35 +20,30 @@ const CreateTrade: NextPage = () => {
   const [windowState, setWindowState] = useState<Window>();
 
   useEffect(() => {
-    if (window) {
-      window.Telegram = window.Telegram || {};
-      setWindowState(window);
+    if (window.location.search) {
+      const urlParams = new URLSearchParams(window.location.search);
+  
+      const userId = urlParams.get("user_id") as string;
+      const name = urlParams.get("name") as string;
+      const username = urlParams.get("username") as string;
+      const languageCode = urlParams.get("language_code") as string;
+      const queryId = urlParams.get("query_id") as string;
+  
+      console.log(userId, name, username, languageCode, queryId);
+      setState({
+        userId,
+        name,
+        username,
+        languageCode,
+        queryId,
+      });
     }
-    // if (window.location.search) {
-    //   const urlParams = new URLSearchParams(window.location.search);
-  
-    //   const userId = urlParams.get("user_id") as string;
-    //   const name = urlParams.get("name") as string;
-    //   const username = urlParams.get("username") as string;
-    //   const languageCode = urlParams.get("language_code") as string;
-    //   const queryId = urlParams.get("query_id") as string;
-  
-    //   console.log(userId, name, username, languageCode, queryId);
-    //   setState({
-    //     userId,
-    //     name,
-    //     username,
-    //     languageCode,
-    //     queryId,
-    //   });
-    // }
   }, []);
 
   return (
     <div className="relative w-full overflow-hidden flex flex-col items-center justify-center">
       <div className="rounded-xl w-full bg-dark-gray shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] box-border overflow-y-auto flex flex-col p-[18px] items-center justify-start gap-[9px] border-[2.5px] border-solid border-zinc-700">
         {JSON.stringify(state)}
-        {JSON.stringify(windowState)}
 
         <div className="flex w-full md:w-3/4 lg:w-1/2 flex-col p-[9px] items-center justify-start gap-[18px]">
           <BuyAllHeaderDiv />
