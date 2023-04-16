ef9f80d85f57e2d0930ba6578967b588f1eb7d3b
msubhan9803 <mohammadsubhan9803@gmail.com>
2023-04-16 06:05:53 +0500

    create trade ui/ux done

---
+++ src/components/trading-currency.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import CreateTradeCurrency from "./create-trade-currency";
+
+export default function CurrencyDisclosuer({
+  id,
+  isOpen,
+  buttonText,
+  currencyList,
+  selectedCurrency,
+  amount,
+  handleDisclosuerClick,
+  handleCurrencySelect,
+  handleAmount
+}: {
+  id: any;
+  isOpen: any;
+  buttonText: any;
+  currencyList: any;
+  selectedCurrency: any;
+  amount: any;
+  handleDisclosuerClick: any;
+  handleCurrencySelect: any;
+  handleAmount: any;
+}) {
+  return (
+    <>
+      <button
+        className="flex flex-col w-full rounded-3xl bg-[#1c1c1c] px-4 py-2 text-left text-sm font-medium text-white focus:outline-none focus-visible:ring focus-visible:ring-opacity-75"
+        onClick={() => handleDisclosuerClick(id)}
+        aria-expanded={isOpen}
+        {...(isOpen && { "aria-controls": id })}
+      >
+        <div className="relative font-medium m-auto">{buttonText}:</div>
+        <div className="relative box-border m-auto w-3/4 my-1 h-px shrink-0 border-t-[1px] border-solid border-gray-600" />
+        <span className="m-auto">Select coin</span>
+      </button>
+      {isOpen && (
+        <div className="px-4 pt-4 pb-2 text-sm text-gray-500 bg-[#1c1c1c] rounded-3xl">
+          <CreateTradeCurrency
+            amount={amount}
+            selectedCurrency={selectedCurrency}
+            currencyList={currencyList}
+            handleCurrencySelect={handleCurrencySelect}
+            handleAmount={handleAmount}
+          />
+        </div>
+      )}
+    </>
+  );
+}
--- src/pages/create-trade.tsx
+++ src/pages/create-trade.tsx
@@ -1,25 +1,55 @@
 import React, { useState } from "react";
-import { ChevronUpIcon } from "@heroicons/react/20/solid";
-import CreateTradeCurrency from "@/components/create-trade-currency";
+import CurrencyDisclosuer from "@/components/trading-currency";
+
+const currencyList = [
+  {
+    label: "BTC",
+    image: "/btc.svg",
+  },
+  {
+    label: "ETH",
+    image: "/eth.svg",
+  },
+  {
+    label: "BUSD",
+    image: "/busd.svg",
+  },
+  {
+    label: "TEST",
+    image: "/coin-10-1.svg",
+  },
+  {
+    label: "TEST",
+    image: "/coin-11-1.svg",
+  },
+  {
+    label: "TEST",
+    image: "/coin-7-1.svg",
+  },
+  {
+    label: "TEST",
+    image: "/coin-2-1.svg",
+  },
+  {
+    label: "TEST",
+    image: "/coin-15-1.svg",
+  },
+  {
+    label: "TEST",
+    image: "/coin-12-1.svg",
+  },
+];
 
 export default function CreateTrade() {
-  const [disclosures, setDisclosures] = useState([
-    {
-      id: "disclosure-panel-1",
-      isOpen: true,
-      buttonText: "Trading",
-      panelText:
-        "If you're unhappy with your purchase for any reason, email us within 90 days and we'll refund you in full, no questions asked.",
-    },
-    {
-      id: "disclosure-panel-2",
-      isOpen: false,
-      buttonText: "Seeking",
-      panelText: "No.",
-    },
-  ]);
+  const [selectedTradingCurrency, setSelectedTradingCurrency] =
+    useState<number>(-1);
+  const [selectedSeekingCurrency, setSelectedSeekingCurrency] =
+    useState<number>(-1);
 
-  const handleClick = (id: any) => {
+  const [tradingAmount, setTradingAmount] = useState(0)
+  const [seekingAmount, setSeekingAmount] = useState(0)
+
+  const handleDisclosuerClick = (id: any) => {
     setDisclosures(
       disclosures.map((d) =>
         d.id === id ? { ...d, isOpen: !d.isOpen } : { ...d, isOpen: false }
@@ -27,27 +57,52 @@ export default function CreateTrade() {
     );
   };
 
+  const handleTradingCurrencySelect = (index: number) => {
+    setSelectedTradingCurrency(index);
+  };
+
+  const handleSeekingCurrencySelect = (index: number) => {
+    setSelectedSeekingCurrency(index);
+  };
+
+  const handleTradingAmount = (e: any) => setTradingAmount(e.target.value);
+
+  const handleSeekingAmount = (e: any) => setSeekingAmount(e.target.value);
+
+  const [disclosures, setDisclosures] = useState([
+    {
+      id: "disclosure-panel-1",
+      isOpen: true,
+      buttonText: "Trading",
+      handleCurrencySelect: handleTradingCurrencySelect,
+      handleAmount: handleTradingAmount,
+      type: 'trading'
+    },
+    {
+      id: "disclosure-panel-2",
+      isOpen: false,
+      buttonText: "Seeking",
+      handleCurrencySelect: handleSeekingCurrencySelect,
+      handleAmount: handleSeekingAmount,
+      type: 'seeking'
+    }
+  ]);
+
   return (
     <div className="mx-auto w-full max-w-md rounded-3xl p-2 space-y-2 bg-[#1e69a0]">
-      {disclosures.map(({ id, isOpen, buttonText, panelText }) => (
-        <React.Fragment key={id}>
-          <button
-            className="flex flex-col w-full rounded-3xl bg-[#1c1c1c] px-4 py-2 text-left text-sm font-medium text-white focus:outline-none focus-visible:ring focus-visible:ring-opacity-75"
-            onClick={() => handleClick(id)}
-            aria-expanded={isOpen}
-            {...(isOpen && { "aria-controls": id })}
-          >
-            <div className="relative font-medium m-auto">{buttonText}:</div>
-            <div className="relative box-border m-auto w-3/4 my-1 h-px shrink-0 border-t-[1px] border-solid border-gray-600" />
-            <span className="m-auto">Select coin</span>
-          </button>
-          {isOpen && (
-            <div className="px-4 pt-4 pb-2 text-sm text-gray-500 bg-[#1c1c1c] rounded-3xl">
-              {/* {panelText} */}
-              <CreateTradeCurrency />
-            </div>
-          )}
-        </React.Fragment>
+      {disclosures.map(({ id, isOpen, buttonText, handleCurrencySelect, handleAmount, type }, index) => (
+        <CurrencyDisclosuer
+          key={index}
+          id={id}
+          isOpen={isOpen}
+          buttonText={buttonText}
+          currencyList={currencyList}
+          selectedCurrency={type === 'trading' ? selectedTradingCurrency : selectedSeekingCurrency}
+          amount={type === 'trading' ? tradingAmount : seekingAmount}
+          handleDisclosuerClick={handleDisclosuerClick}
+          handleCurrencySelect={handleCurrencySelect}
+          handleAmount={handleAmount}
+        />
       ))}
     </div>
   );
