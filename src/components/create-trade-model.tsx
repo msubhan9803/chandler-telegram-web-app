2e78b3af1491eaaf0f3a72284c8108e8c30bb497
msubhan9803 <mohammadsubhan9803@gmail.com>
2023-06-05 15:13:05 +0500

    feat: modal height adjusted

--- src/components/create-trade-model.tsx
+++ src/components/create-trade-model.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import Modal from "@/components/animated-modal";
 import CreateTradeComponent from "@/components/create-trade-component";
@@ -27,6 +27,14 @@ export default function CreateTradeModel({ isOpen, closeModal }: any) {
   return (
     <Modal isOpen={isOpen} onClose={closeModal}>
       <Dialog.Panel className="w-full lg:m-auto lg:max-w-md transform overflow-hidden bg-custom-second-blue rounded-2xl text-left align-middle shadow-xl transition-all relative">
+        {/* <div className="flex justify-end m-2">
+          <button
+            className="rounded-full hover:bg-opacity-50 hover:bg-gray-600 transition cursor-pointer z-50"
+            onClick={closeModal}
+          >
+            <XMarkIcon className="h-5 w-5 text-gray-300 cursor-pointer" />
+          </button>
+        </div> */}
         <CreateTradeComponent
           state={state}
           selectedTradingCurrency={selectedTradingCurrency}
